532fe9bb1c6354ae91a011fcf7307a6d3be39c84
Anna <anna4776497@gmail.com>
2022-09-28 10:51:19 +0300

    исправлена разметка

--- src/App.js
+++ src/App.js
@@ -1,59 +1,78 @@
-import './App.css';
-import Watch from './components/watch/Watch';
-import { useState } from 'react';
-import { nanoid } from 'nanoid'
-
+import "./App.css";
+import Watch from "./components/watch/Watch";
+import { useState } from "react";
+import { nanoid } from "nanoid";
 
 const App = () => {
-  const [watches, setWatches] = useState([{ cityName: 'Москва', timeZone: '3', id: nanoid() }]);
-    
-   const  onSubmit = (evt) => {
-        evt.preventDefault()
-        const cityName = evt.target.cityName.value;
-        const timeZone = +evt.target.timeZone.value;
-        const item = {
-          cityName: cityName,
-          timeZone: +timeZone,
-          id: nanoid()
-        }
-     if (cityName.length > 1 && timeZone > -13 && timeZone < 13) {
-       evt.target.cityName.value = '';
-       evt.target.timeZone.value = '';
-           setWatches((prev) => [...prev, item]);
-      }
-   }
-  
-    const onDelete = (id) => {
-    setWatches((prevWatches) => prevWatches.filter((item) => item.id !== id))
-    }
-  
-  return (   
-    <div className='container'>
+  const [watches, setWatches] = useState([
+    { cityName: "Москва", timeZone: "3", id: nanoid() },
+  ]);
 
-            <div className='form-container'>
-            <form className='form-input' onSubmit={onSubmit}>
-            <div className='form-block'>
+  const onSubmit = (evt) => {
+    evt.preventDefault();
+    const cityName = evt.target.cityName.value;
+    const timeZone = +evt.target.timeZone.value;
+    const item = {
+      cityName: cityName,
+      timeZone: +timeZone,
+      id: nanoid(),
+    };
+    if (cityName.length > 1 && timeZone > -13 && timeZone < 13) {
+      evt.target.cityName.value = "";
+      evt.target.timeZone.value = "";
+      setWatches((prev) => [...prev, item]);
+    }
+  };
+
+  const onDelete = (id) => {
+    setWatches((prevWatches) => prevWatches.filter((item) => item.id !== id));
+  };
+
+  return (
+    <div className="container">
+      <div className="form-container">
+        <form className="form-input" onSubmit={onSubmit}>
+          <div className="form-block">
             <label htmlFor="name">Название</label>
-            <input placeholder='Например, Москва' id="cityName" name="cityName" />
-            </div>
-                
-            <div className='form-block'>
+            <input
+              placeholder="Например, Москва"
+              id="cityName"
+              name="cityName"
+            />
+          </div>
+
+          <div className="form-block">
             <label htmlFor="time-zone">Временная зона</label>
-            <input placeholder='-3' type='number' id="timeZone" name="timeZone"/>             
-                    </div>
-                     <button type="submit" className='form-block-btn'>Добавить</button>  
-                </form>
+            <input
+              placeholder="-3"
+              type="number"
+              id="timeZone"
+              name="timeZone"
+            />
+          </div>
+          <button type="submit" className="form-block-btn">
+            Добавить
+          </button>
+        </form>
       </div>
-      <div className='wathces-block'>
+      <div className="wathces-block">
         {watches.map((item) => {
-          return <Watch timeZone={item.timeZone} key={item.id} cityName={item.cityName} onDelete={onDelete} id={item.id} />
+          return (
+            <Watch
+              timeZone={item.timeZone}
+              key={item.id}
+              cityName={item.cityName}
+              onDelete={onDelete}
+              id={item.id}
+            />
+          );
         })}
-        {watches.length < 1 ? 'Введите город и разницу по времени в формате UDC' : null}
+        {watches.length < 1
+          ? "Введите город и разницу по времени в формате UDC"
+          : null}
       </div>
     </div>
-  )
-  
-  }
-
+  );
+};
 
 export default App;
--- src/components/watch/Watch.js
+++ src/components/watch/Watch.js
@@ -1,71 +1,82 @@
-import {Component} from 'react'
-import './watch.css'
+import { Component } from "react";
+import "./watch.css";
 
-class Watch extends Component{
-    constructor(props) {
-        super(props)
-        this.cityName = this.props.cityName;
-        this.timeZone = this.props.timeZone;
-        this.onDelete = this.props.onDelete;
-        this.state = {
-        hh: null,
-        mm: null,
-        ss: null
-     }
-    }
+class Watch extends Component {
+  constructor(props) {
+    super(props);
+    this.cityName = this.props.cityName;
+    this.timeZone = this.props.timeZone;
+    this.onDelete = this.props.onDelete;
+    this.state = {
+      hh: null,
+      mm: null,
+      ss: null,
+    };
+  }
 
-    onUpdateClock = () => {
-            const deg = 6;
-            let day = new Date();
-            const cz = day.getTimezoneOffset()/60
-            let hh = (day.getHours() + (cz > 0 ? -cz : cz) + this.props.timeZone) * 30;
-            let mm = day.getMinutes() * deg;
-            let ss = day.getSeconds() * deg;
+  onUpdateClock = () => {
+    const deg = 6;
+    let day = new Date();
+    const cz = day.getTimezoneOffset() / 60;
+    let hh = (day.getHours() + (cz > 0 ? -cz : cz) + this.props.timeZone) * 30;
+    let mm = day.getMinutes() * deg;
+    let ss = day.getSeconds() * deg;
 
-            this.setState({
-            hh,
-            mm,
-            ss
-            })
-    }
+    this.setState({
+      hh,
+      mm,
+      ss,
+    });
+  };
 
-    componentDidMount() {
-        this.timerId = setInterval(() => this.onUpdateClock(), 1000)
-    }
+  componentDidMount() {
+    this.timerId = setInterval(() => this.onUpdateClock(), 1000);
+  }
 
-    componentWillUnmount() {
-        clearInterval(this.timerId, this.props.id)
-    }
-    
-   
+  componentWillUnmount() {
+    clearInterval(this.timerId, this.props.id);
+  }
 
-    render() {
-        const { hh, mm, ss } = this.state
-        return (
-            <div className='clock-elem'>
-                 <span className='city-name'>{this.props.cityName}</span>
-                <span className="material-symbols-outlined" onClick={() => {
-                    this.props.onDelete(this.props.id)
-                }}>
-                 cancel
-                </span>
+  render() {
+    const { hh, mm, ss } = this.state;
+    return (
+      <div className="clock-elem">
+        <span className="city-name">{this.props.cityName}</span>
+        <span
+          className="material-symbols-outlined"
+          onClick={() => {
+            this.props.onDelete(this.props.id);
+          }}
+        >
+          cancel
+        </span>
 
-                 <div className="clock">
-                <div className="hour">
-                <div className="hr" id="hr" style={{'transform': `rotateZ(${(hh) + (mm/12)}deg)`}}></div>
-                </div>
-                <div className="min">
-                    <div className="mn" id="mn" style={{ 'transform': `rotateZ(${mm}deg)` }}></div>
-                </div>
-                <div className="sec">
-                <div className="sc" id="sc" style={{ 'transform': `rotateZ(${ss}deg)` }}></div>
-                </div>
-                </div>
-                </div>
-        )
-        
-    }
-
+        <div className="clock">
+          <div className="hour">
+            <div
+              className="hr"
+              id="hr"
+              style={{ transform: `rotateZ(${hh + mm / 12}deg)` }}
+            ></div>
+          </div>
+          <div className="min">
+            <div
+              className="mn"
+              id="mn"
+              style={{ transform: `rotateZ(${mm}deg)` }}
+            ></div>
+          </div>
+          <div className="sec">
+            <div
+              className="sc"
+              id="sc"
+              style={{ transform: `rotateZ(${ss}deg)` }}
+            ></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Watch
+export default Watch;
